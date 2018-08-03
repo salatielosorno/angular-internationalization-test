14e56c42158555ebecf612a2aa6b91dd316109ee
Salatiel Osorno <sosorno@marathongroup.mx>
2018-08-03 15:17:01 -0500

    This is a test about internationalization in Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 
+
+import { registerLocaleData } from '@angular/common';
+import localES from '@angular/common/locales/es';
+
+
+registerLocaleData(localES, 'es-MX');
 
 @NgModule({
   declarations: [
